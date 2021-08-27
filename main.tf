d924d96f6c25a6d86ef581525ce0377d51ee141a
MuralidharKumar <87074317+MuralidharKumar@users.noreply.github.com>
2021-08-27 21:32:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,17 @@
-provider "azurerm" {
-  features {}
-}
-
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.64.0"
+    required_providers{
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~>2.45"
+        }
     }
-  }
 }
-
 provider "azurerm" {
-  features {}
-  alias           = "corenetwork"
-  subscription_id = var.network_subscription_id
+    features {}
 }
 
-data "terraform_remote_state" "network" {
-  backend = "remote"
-  config = {
-    organization = var.tfe_org_name
-    hostname     = var.tfe_host_name
-    workspaces = {
-      name = var.tfe_network_workspace_name
-    }
-  }
-}
-
-data "azurerm_network_watcher" "network_watcher" {
-  name                = "NetworkWatcher_${var.region_primary}"
-  resource_group_name = "NetworkWatcherRG"
+resource "azurerm_resource_group" "test-rg" {
+    name = var.rg_name
+    location = var.location
+    
 }
