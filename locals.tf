0b8f3ac55df1cb1c0fa959799a486d45f67e5438
MuralidharKumar <87074317+MuralidharKumar@users.noreply.github.com>
2021-08-27 21:40:13 +0530

    Delete locals.tf

--- locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  platform_environment = "stage"
-  region               = "centralus"
-  environment          = "prod"
-  vnet_name            = "logging"
-  account_type         = "core"
-}
